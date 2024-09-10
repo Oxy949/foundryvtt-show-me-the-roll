a3aa18be69ef43744601c9f3d8331fcceb06850e
Konstantin Kustov <oxy949@gmail.com>
2024-09-10 20:33:16 +0300

    Add 1 ms delay

--- scripts/module.js
+++ scripts/module.js
@@ -56,29 +56,31 @@ Hooks.once('ready', async function() {
 
 Hooks.on("renderChatMessage", function (message, html, data) {
     // Check if the message is a roll
-    if (message.isRoll || (message.rolls && message.rolls.length > 0)) {
-        if (game.settings.get("foundryvtt-show-me-the-roll", "autoExpand")) {
-            html.find('.dice-roll').addClass('expanded');
-            html.find('.dice-roll .dice-tooltip').addClass('expanded');
-        }
+    if (message.isRoll) {
+        setTimeout(() => {
+            if (game.settings.get("foundryvtt-show-me-the-roll", "autoExpand")) {
+                html.find('.dice-roll').addClass('expanded');
+                html.find('.dice-roll .dice-tooltip').addClass('expanded');
+            }
         
-        if (game.settings.get("foundryvtt-show-me-the-roll", "hideRollFormula")) {
-            html.find('.dice-formula').css('visibility', 'hidden');
-            html.find('.dice-formula').css('position', 'absolute');
-        }
-
-        if (game.settings.get("foundryvtt-show-me-the-roll", "hideSmallResult")) {
-            html.find('.dice-tooltip .total').css('visibility', 'hidden');
-            html.find('.dice-tooltip .total').css('position', 'absolute');
-        }
-
-        if (game.settings.get("foundryvtt-show-me-the-roll", "hideBottomResult")) {
-            html.find('.dice-total').css('visibility', 'hidden');
-            html.find('.dice-total').css('position', 'absolute');
-        }
-
-        if (game.settings.get("foundryvtt-show-me-the-roll", "disableClickInteraction")) {
-            html.find('.dice-roll').addClass('no-interaction');
-        }
+            if (game.settings.get("foundryvtt-show-me-the-roll", "hideRollFormula")) {
+                html.find('.dice-formula').css('visibility', 'hidden');
+                html.find('.dice-formula').css('position', 'absolute');
+            }
+    
+            if (game.settings.get("foundryvtt-show-me-the-roll", "hideSmallResult")) {
+                html.find('.dice-tooltip .total').css('visibility', 'hidden');
+                html.find('.dice-tooltip .total').css('position', 'absolute');
+            }
+    
+            if (game.settings.get("foundryvtt-show-me-the-roll", "hideBottomResult")) {
+                html.find('.dice-total').css('visibility', 'hidden');
+                html.find('.dice-total').css('position', 'absolute');
+            }
+    
+            if (game.settings.get("foundryvtt-show-me-the-roll", "disableClickInteraction")) {
+                html.find('.dice-roll').addClass('no-interaction');
+            }
+        }, 1); // Delay of 1 milliseconds
     }
 });
