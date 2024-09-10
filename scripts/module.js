077d3fad16f0577e918446a7a29840edaf1bb3d2
Konstantin Kustov <oxy949@gmail.com>
2024-09-10 15:26:07 +0300

    Update readme and improve UI

--- scripts/module.js
+++ scripts/module.js
@@ -14,5 +14,11 @@ Hooks.on("renderChatMessage", function (message, html, data) {
         html.find('.dice-tooltip .tooltip-part .total').css('visibility', 'hidden');
         html.find('.dice-total').css('visibility', 'hidden');
         html.find('.dice-total').css('height', '0px');
+        html.find('.dice-total').css('visibility', 'hidden');
+        html.find('.dice-total').css('visibility', 'hidden');
+        html.find('.dice-tooltip .tooltip-part .total').css('visibility', 'hidden');
+        html.find('.dice-tooltip .tooltip-part .total').css('width', '0');
+        html.find('.dice-tooltip .tooltip-part .total').css('height', '0');
+        html.find('.dice-tooltip .tooltip-part .total').css('position', 'absolut');
     }
 });
