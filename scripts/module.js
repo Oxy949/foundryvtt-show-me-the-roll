1cb93e5aa594ef36b11bccc52282393990a2be78
Konstantin Kustov <oxy949@gmail.com>
2024-09-10 14:47:18 +0300

    Add base logic

--- scripts/module.js
+++ scripts/module.js
@@ -5,3 +5,14 @@ Hooks.once('init', async function() {
 Hooks.once('ready', async function() {
 
 });
+
+Hooks.on("renderChatMessage", function (message, html, data) {
+    // Check if the message is a roll
+    if (message.isRoll) {
+        html.find('.dice-roll').addClass('expanded');
+        html.find('.dice-tooltip .tooltip-part').css('margin-bottom', '0px');
+        html.find('.dice-tooltip .tooltip-part .total').css('visibility', 'hidden');
+        html.find('.dice-total').css('visibility', 'hidden');
+        html.find('.dice-total').css('height', '0px');
+    }
+});
